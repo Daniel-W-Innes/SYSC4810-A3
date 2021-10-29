b24aacd24c9e73ec646944a0deab0101172eb3ff
daniel <daniel.w.innes@gmail.com>
2021-10-29 16:52:18 -0400

    docker rum --rm

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ if docker ps -a | grep -q 'a3'; then
    docker rm -f a3
 fi
 # shellcheck disable=SC2046
-docker run --name a3 -v $(pwd)/sensitive_files/:/sensitive_files/ -it a3
+docker run --name a3 -v $(pwd)/sensitive_files/:/sensitive_files/ -it --rm a3
