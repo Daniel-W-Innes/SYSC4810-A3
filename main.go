416e9902e304d5254be62d8fb1bab6fe4b5a1811
Daniel Innes <Daniel.W.Innes@gmail.com>
2021-10-29 12:03:49 -0400

    docs

--- main.go
+++ main.go
@@ -29,52 +29,63 @@ type User struct {
 	password []byte
 }
 
+//Create a user structs from the nested arrays extracted from the passwd file.
 func arrayToUsers(records [][]string) map[string]UserRecord {
 	output := make(map[string]UserRecord)
 	var user UserRecord
 	for _, record := range records {
+		//This could be made more complicated by parsing headers, but it's not necessary for this assignment.
 		user = UserRecord{record[0], record[1], record[2], record[3]}
 		output[user.username] = user
 	}
 	return output
 }
 
-func getUsers(passwordFile string) (map[string]UserRecord, error) {
-	file, err := os.Open(passwordFile)
+//Open a CSV file, read its contents to nested arrays, then close it.
+func readCSV(fileName string) ([][]string, error) {
+	//Open the csv file
+	file, err := os.Open(fileName)
 	if err != nil {
 		return nil, err
 	}
+	//Set file to close after loading is done
 	defer func(file *os.File) {
 		err := file.Close()
 		if err != nil {
 			log.Panic()
 		}
 	}(file)
+	//Read and decode the csv in to nested arrays
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
 	if err != nil {
 		return nil, err
 	}
+	return records, nil
+}
+
+//Get system users
+func getUsers(passwordFile string) (map[string]UserRecord, error) {
+	//Get CSV file contents
+	records, err := readCSV(passwordFile)
+	if err != nil {
+		return nil, err
+	}
+	//Convert nested arrays to users
 	return arrayToUsers(records), nil
 }
 
+//Get relationships between patients and doctors.
+//Formatted with the doctor userId as the key and an array of patient userIds as the value.
 func getPatients(patientsFile string) (map[string][]string, error) {
 	output := make(map[string][]string)
-	file, err := os.Open(patientsFile)
+	//Get CSV file contents
+	records, err := readCSV(patientsFile)
 	if err != nil {
 		return nil, err
 	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			log.Panic()
-		}
-	}(file)
-	reader := csv.NewReader(file)
-	records, err := reader.ReadAll()
-	if err != nil {
-		return nil, err
-	}
+	//Convert nested arrays to map of arrays
+	//e.g. [[1,2],[1,4],[2,5]] -> {1:[2,4],2:[5]}
 	for _, record := range records {
 		if user, ok := output[record[0]]; ok {
 			output[record[0]] = append(user, record[1])
@@ -85,16 +96,20 @@ func getPatients(patientsFile string) (map[string][]string, error) {
 	return output, nil
 }
 
+//Get login credentials from user input
 func getCredentials(hidePassword bool) (User, error) {
 	var username string
 	var password []byte
+	//Get username from user
 	fmt.Print("Enter username: ")
 	_, err := fmt.Scanln(&username)
 	if err != nil {
 		return User{}, err
 	}
+	//Get password from user
 	fmt.Print("Enter password: ")
 	if hidePassword {
+		//If enabled hide the password as the user is typing it. this is not operating system independent.
 		password, err = term.ReadPassword(int(os.Stdin.Fd()))
 	} else {
 		_, err = fmt.Scanln(&password)
@@ -103,43 +118,52 @@ func getCredentials(hidePassword bool) (User, error) {
 		return User{}, err
 	}
 	fmt.Println()
+	//Renew return username and password as user struct
 	return User{username: username, password: password}, nil
 }
 
+//Generate passwd file formatted as a CSV from user input.
 func generatePasswd(passwordFile string, policy *strength.PasswordPolicy, hidePassword bool) error {
+	//Create empty password file
 	file, err := os.Create(passwordFile)
 	if err != nil {
 		return err
 	}
+	//Setup writer to write users into password file
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 	var role string
 	var userInput string
 	c := true
-	i := 0
+	userID := 0
 	for c {
+		//Get credentials for a new user
 		user, err := getCredentials(hidePassword)
 		if err != nil {
 			return err
 		}
 		passwordKeyChan := make(chan []byte)
 		errorChan := make(chan error)
+		//Start hashing password concurrently, to main process
 		go func(user User) {
 			passwordKey, err := bcrypt.GenerateFromPassword(preHash(user.password), bcryptCost)
 			passwordKeyChan <- passwordKey
 			errorChan <- err
 		}(user)
+		//Add a new password policy to ensure that the user cannot have a password of his own username
 		(*policy).TempPolices = append((*policy).TempPolices, &strength.ProhibitedPasswordsPolicy{ProhibitedPasswords: map[string]bool{user.username: true}})
 		err = (*policy).Check(string(user.password))
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
+		//Get the new users role
 		fmt.Print("Enter role: ")
 		_, err = fmt.Scanln(&role)
 		if err != nil {
 			return err
 		}
+		//Get password hash from concurrent thread
 		passwordKey := <-passwordKeyChan
 		err = <-errorChan
 		close(passwordKeyChan)
@@ -147,29 +171,38 @@ func generatePasswd(passwordFile string, policy *strength.PasswordPolicy, hidePa
 		if err != nil {
 			return err
 		}
-		err = writer.Write([]string{user.username, string(passwordKey), strconv.Itoa(i), role})
+		//Write user into file.
+		//Itoa converts integers to strings
+		err = writer.Write([]string{user.username, string(passwordKey), strconv.Itoa(userID), role})
 		if err != nil {
 			return err
 		}
+		//Check if the user wants to input more users
 		fmt.Print("Do you want to continue (y/n)? ")
 		_, err = fmt.Scanln(&userInput)
 		if err != nil {
 			return err
 		}
 		c = userInput == "y"
-		i++
+		userID++
 	}
 	return nil
 }
 
+//Prehash and encode the user inputted password
 func preHash(password []byte) []byte {
+	//Setup to hash the password with the pepper key as the secret
 	hashedPassword := hmac.New(sha256.New, []byte(os.Getenv("PEPPER_KEY")))
 	hashedPassword.Write(password)
+	//Encode the resulting hash as Base64
 	return []byte(base64.StdEncoding.EncodeToString(hashedPassword.Sum(nil)))
 }
 
+//Check the user inputted password against the password key.
+//Return the user's record and true if the user successfully logged-in.
 func checkPassword(user User, users map[string]UserRecord) (UserRecord, bool) {
 	if record, ok := users[user.username]; ok {
+		//Prehash the password and then check it against the password key
 		if bcrypt.CompareHashAndPassword([]byte(record.passwordKey), preHash(user.password)) == nil {
 			return record, true
 		}
@@ -177,11 +210,15 @@ func checkPassword(user User, users map[string]UserRecord) (UserRecord, bool) {
 	return UserRecord{}, false
 }
 
+//Generate password policy from information provided in assignment.
+//This uses the strength module defined in the same repo.
 func getPasswordPolicy() *strength.PasswordPolicy {
+	//Load prohibited passwords from prohibitive password file
 	prohibitedPasswords, err := strength.GetProhibitedPasswords(bassPath + "/prohibited_passwords")
 	if err != nil {
 		return nil
 	}
+	//Load prohibited regex from prohibited regex file
 	prohibitedRegexes, err := strength.GetProhibitedRegexes(bassPath + "/prohibited_regexes")
 	if err != nil {
 		return nil
@@ -199,6 +236,7 @@ func getPasswordPolicy() *strength.PasswordPolicy {
 func main() {
 	hidePassword := os.Getenv("HIDE_PASSWORD") != "n"
 	passwordPolicy := getPasswordPolicy()
+	//check environment variable to see if a password file should be generated
 	if os.Getenv("GENERATE_PASSWD") == "y" {
 		err := generatePasswd(bassPath+"/passwd", passwordPolicy, hidePassword)
 		if err != nil {
@@ -208,6 +246,7 @@ func main() {
 
 	usersChan := make(chan map[string]UserRecord)
 	patientsMapChan := make(chan map[string][]string)
+	//Load password file and patient's file Concurrently, to main process
 	go func() {
 		users, err := getUsers(bassPath + "/passwd")
 		if err != nil {
@@ -224,10 +263,12 @@ func main() {
 	fmt.Print("\n\nMedView Imaging\n" +
 		"Medical Information Management System\n" +
 		"-----------------------------------------------------\n")
+	//Get login info from the user
 	user, err := getCredentials(hidePassword)
 	if err != nil {
 		log.Panic("failed get credentials")
 	}
+	//Check user login info
 	record, ok := checkPassword(user, <-usersChan)
 	if ok {
 		fmt.Println("ACCESS GRANTED")
@@ -235,6 +276,7 @@ func main() {
 		fmt.Println("Bad username or password")
 		return
 	}
+	//Print out user's permissions
 	if patients, ok := (<-patientsMapChan)[record.userId]; ok {
 		fmt.Printf("User permissions:\n role: %s\n patient IDs: %s\n", record.role, patients)
 	} else {
--- strength/strength.go
+++ strength/strength.go
@@ -9,15 +9,18 @@ import (
 	"unicode"
 )
 
+// PasswordPolicy Class for checking passwords against predefined password policy
 type PasswordPolicy struct {
 	SubPolices  []SubPolicy
 	TempPolices []SubPolicy
 }
 
+// SubPolicy Generic sub policy for one specific role of a password policy
 type SubPolicy interface {
 	check(s string) error
 }
 
+// CasePolicy Minimum number of uppercase and lowercase letters
 type CasePolicy struct {
 	MinUpper int
 	MinLower int
@@ -26,6 +29,7 @@ type CasePolicy struct {
 func (policy *CasePolicy) check(s string) error {
 	numUpper := 0
 	numLower := 0
+	//Loop over spring counting uppercase and lowercase letters
 	for _, character := range s {
 		if unicode.IsLetter(character) {
 			if unicode.IsUpper(character) {
@@ -34,10 +38,12 @@ func (policy *CasePolicy) check(s string) error {
 				numLower++
 			}
 		}
+		//Return once enough letters of each case have been found
 		if numLower >= (*policy).MinLower && numUpper >= (*policy).MinUpper {
 			return nil
 		}
 	}
+	//Generate error message
 	if numLower < (*policy).MinLower && numUpper < (*policy).MinUpper {
 		return errors.New("not enough lowercase and uppercase letters")
 	} else if numLower < (*policy).MinLower {
@@ -47,49 +53,60 @@ func (policy *CasePolicy) check(s string) error {
 	}
 }
 
+// NumberPolicy Minimum number of numbers
 type NumberPolicy struct {
 	MinNumbers int
 }
 
 func (policy *NumberPolicy) check(s string) error {
 	numNumbers := 0
+	//Loop over spring counting numbers
 	for _, character := range s {
 		if unicode.IsNumber(character) {
 			numNumbers++
 		}
+		//Return once enough numbers have been found
 		if numNumbers >= (*policy).MinNumbers {
 			return nil
 		}
 	}
+	//Generate error message
 	return errors.New("not enough numbers")
 }
 
+// SpecialPolicy Minimum number of predefined special characters
 type SpecialPolicy struct {
 	MinSpecial        int
-	SpecialCharacters map[rune]bool
+	SpecialCharacters map[rune]bool //The bool does not matter, it is just a cheap way of doing hash-based checking
 }
 
 func (policy *SpecialPolicy) check(s string) error {
 	numSpecial := 0
+	//Loop over spring counting special characters
 	for _, character := range s {
 		if _, special := (*policy).SpecialCharacters[character]; special {
 			numSpecial++
 		}
+		//Return once enough special characters have been found
 		if numSpecial >= (*policy).MinSpecial {
 			return nil
 		}
 	}
+	//Generate error message
 	return errors.New("not enough special letters")
 }
 
+// LengthPolicy Minimum and maximum password length
 type LengthPolicy struct {
-	MaxLength int
+	MaxLength int //This is not recommended but required by the assignment
 	MinLength int
 }
 
 func (policy *LengthPolicy) check(s string) error {
+	//Check if password is too short
 	if len(s) < (*policy).MinLength {
 		return errors.New("password is too short")
+		//Check if password is too long
 	} else if len(s) > (*policy).MaxLength {
 		return errors.New("password is too long")
 	} else {
@@ -97,11 +114,13 @@ func (policy *LengthPolicy) check(s string) error {
 	}
 }
 
+// ProhibitedPasswordsPolicy Predefined prohibited passwords
 type ProhibitedPasswordsPolicy struct {
-	ProhibitedPasswords map[string]bool
+	ProhibitedPasswords map[string]bool //The bool does not matter, it is just a cheap way of doing hash-based checking
 }
 
 func (policy *ProhibitedPasswordsPolicy) check(s string) error {
+	//Check if the password is prohibited
 	if _, prohibited := (*policy).ProhibitedPasswords[s]; prohibited {
 		return errors.New("this password is prohibited")
 	} else {
@@ -109,18 +128,22 @@ func (policy *ProhibitedPasswordsPolicy) check(s string) error {
 	}
 }
 
+// GetProhibitedPasswords Load prohibited password file into a sub policy
 func GetProhibitedPasswords(prohibitedPasswordsFile string) (*ProhibitedPasswordsPolicy, error) {
 	prohibitedPolicy := ProhibitedPasswordsPolicy{ProhibitedPasswords: make(map[string]bool)}
+	//Open prohibited password file
 	file, err := os.Open(prohibitedPasswordsFile)
 	if err != nil {
 		return nil, err
 	}
+	//Set file to close after loading is done
 	defer func(file *os.File) {
 		err := file.Close()
 		if err != nil {
 			log.Panic()
 		}
 	}(file)
+	//Load prohibited password from lines in the files
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		prohibitedPolicy.ProhibitedPasswords[scanner.Text()] = true
@@ -131,6 +154,8 @@ func GetProhibitedPasswords(prohibitedPasswordsFile string) (*ProhibitedPassword
 	return &prohibitedPolicy, nil
 }
 
+// ProhibitedRegexesPolicy Predefined prohibited regexes patterns.
+// Useful for preventing phone numbers, postal codes, license plates, etc.
 type ProhibitedRegexesPolicy struct {
 	ProhibitedRegexes []*regexp.Regexp
 }
@@ -169,6 +194,7 @@ func (policy *ProhibitedRegexesPolicy) check(s string) error {
 	}
 	return nil
 }
+
 func runSubPolices(polices *[]SubPolicy, s string) error {
 	for _, subPolices := range *polices {
 		err := subPolices.check(s)
