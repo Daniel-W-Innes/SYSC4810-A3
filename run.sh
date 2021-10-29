fcf1879fad17fca2becbffc4fb5426a0467c45cf
daniel <daniel.w.innes@gmail.com>
2021-10-29 16:47:07 -0400

    readme

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 docker build -t a3 .
-docker rm -f a3
+if docker ps -a | grep -q 'a3'; then
+   docker rm -f a3
+fi
 # shellcheck disable=SC2046
 docker run --name a3 -v $(pwd)/sensitive_files/:/sensitive_files/ -it a3
