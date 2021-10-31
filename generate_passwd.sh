3350ddae6efd8c26d43fa794e73e8bfb3ff91ee6
Daniel Innes <Daniel.W.Innes@gmail.com>
2021-10-31 15:29:43 -0400

    generate_passwd

---
+++ generate_passwd.sh
@@ -0,0 +1,6 @@
+docker build -t a3 .
+if docker ps -a | grep -q 'a3'; then
+   docker rm -f a3
+fi
+# shellcheck disable=SC2046
+docker run --name a3 -v $(pwd)/sensitive_files/:/sensitive_files/ -it --rm -e GENERATE_PASSWD=y a3
